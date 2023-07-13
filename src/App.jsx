a19d9b96221caeae64ce633ebde4c334a3600891
the-soban <sobanshafeq123@gmail.com>
2023-07-14 00:06:38 +0500

    installed and setup React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParas from "./SearchParas";
 import Details from "./Details";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <header>
-        <Link to="/"></Link>
-      </header>
-      {/* <h1>Adopt Me!</h1> */}
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />{" "}
-        {/* the :id means that id is a varibale  */}
-        <Route path="/" element={<SearchParas />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <header>
+          <Link to="/"></Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />{" "}
+          {/* the :id means that id is a varibale  */}
+          <Route path="/" element={<SearchParas />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
