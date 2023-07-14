607d3074bd4d3eb0e4caac0309a5826ab67afd5f
the-soban <sobanshafeq123@gmail.com>
2023-07-14 22:36:14 +0500

    added react query functionality in Details.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,41 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams(); //useParams gets this id from something called context, which is in App.jsx component
-  return <h1>Pet: {id}</h1>;
+  const results = useQuery(["details", id], fetchPet); //["details", id] will be passed as the queryKey to fetchPet
+
+  if (results.isError) {
+    return (
+      <div>
+        <h2>Oh No! Something went wrong!</h2>
+        <h3>{Error}</h3>
+      </div>
+    );
+  }
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h4 className="loader">+</h4>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+        </h2>
+        <p>{pet.description}</p>
+        <button>Adopt {pet.name}</button>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
