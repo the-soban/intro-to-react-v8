3e54e2c284a353235fae4998a7134e0da16bdc67
the-soban <sobanshafeq123@gmail.com>
2023-07-18 21:18:06 +0500

    created modal component, added it in Details.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams(); //useParams gets this id from something called context, which is in App.jsx component
   const results = useQuery(["details", id], fetchPet); //["details", id] will be passed as the queryKey to fetchPet
 
@@ -35,7 +38,24 @@ const Details = () => {
           {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
         </h2>
         <p>{pet.description}</p>
-        <button>Adopt {pet.name}</button>
+        <button
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          Adopt {pet.name}
+        </button>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
